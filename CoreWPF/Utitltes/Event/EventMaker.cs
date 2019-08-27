87dac07898ac5aef3c1db66a67bdfb49a51416f4
Scorpionsik <scorpionsik@gmail.com>
2019-08-27 13:30:02 +0300

    tmp

---
+++ CoreWPF/Utitltes/Event/EventMaker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoreWPF.Utitltes.Event
+{
+    public static class EventMaker
+    {
+        private static Dictionary<int, EventUnit> events;
+        private static EventWaitHandle locker;
+
+        static EventMaker()
+        {
+            EventMaker.events = new Dictionary<int, EventUnit>();
+            EventMaker.locker = new AutoResetEvent(false);
+        }
+
+        public static int Start(Task job, Action end_action = null)
+        {
+            int curr_id = 0;
+            while (EventMaker.events.ContainsKey(curr_id)) curr_id++;
+
+            EventUnit tmp = new EventUnit(curr_id, job, EventMaker.Delete, end_action);
+            EventMaker.events.Add(curr_id, tmp);
+            EventMaker.events[curr_id].Start();
+            return curr_id;
+        }
+
+        private static void Delete(EventUnit unit)
+        {
+            unit.Event_appFinish?.Invoke();
+            EventMaker.events.Remove(unit.Id);
+            //if (EventMaker.events.Count == 0) EventMaker.locker.Set();
+        }
+
+        public static void WaitForEvent(int id)
+        {
+            if (EventMaker.events.ContainsKey(id)) EventMaker.events[id].WaitForEvent();
+        }
+    }
+}
--- CoreWPF/Utitltes/Event/EventUnit.cs
+++ CoreWPF/Utitltes/Event/EventUnit.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CoreWPF.Utitltes.Event
@@ -6,29 +7,36 @@ namespace CoreWPF.Utitltes.Event
     class EventUnit
     {
         public int Id { get; private set; }
+        //private EventWaitHandle locker;
 
-        private event Action<EventUnit> finish;
-        public Action<EventUnit> Finish
-        {
-            get { return this.finish; }
-            set
-            {
-                this.finish = new Action<EventUnit>(value);
-            }
-        }
-
+        private event Action event_appFinish;
+        private event Action<EventUnit> event_finish;
         private readonly Task job;
 
-        public EventUnit(int id, Task job)
+        public Action Event_appFinish
+        {
+            get { return this.event_appFinish; }
+        }
+
+        public EventUnit(int id, Task job, Action<EventUnit> manager_action, Action end_action)
         {
             this.Id = id;
             this.job = job;
+            this.event_finish += manager_action;
+            this.event_appFinish += end_action;
+            //this.locker = new AutoResetEvent(false);
         }
 
         public async void Start()
         {
             await Task.Run(()=> { this.job.Start(); });
-            await Task.Run(() => { this.Finish?.Invoke(this); });
+            await Task.Run(() => { this.event_finish?.Invoke(this); });
+            //await Task.Run(() => { this.locker.Set(); });
+        }
+
+        public void WaitForEvent()
+        {
+            //this.locker.WaitOne();
         }
     }
 }
