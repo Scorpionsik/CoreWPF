e292ba853004ffcfd0c3dc4eab067fd5c71d9c6d
Scorpionsik <scorpionsik@gmail.com>
2019-08-27 13:34:04 +0300

    Revert "tmp"
    This reverts commit 87dac07898ac5aef3c1db66a67bdfb49a51416f4.

---
+++ CoreWPF/Utitltes/Event/EventManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoreWPF.Utitltes.Event
+{
+    public static class EventManager
+    {
+        private static Dictionary<int, EventUnit> events;
+        public static Mutex IsWork { get; private set; }
+
+        static EventManager()
+        {
+            EventManager.events = new Dictionary<int, EventUnit>();
+            EventManager.IsWork = new Mutex();
+        }
+
+        public static int Start(Task job)
+        {
+            int curr_id = 0;
+            while (EventManager.events.ContainsKey(curr_id)) curr_id++;
+            if (EventManager.events.Count == 0) EventManager.IsWork.WaitOne();
+
+            EventUnit tmp = new EventUnit(curr_id, job);
+            tmp.Finish += new System.Action<EventUnit>(EventManager.Delete);
+            EventManager.events.Add(curr_id, tmp);
+            EventManager.events[curr_id].Start();
+            return curr_id;
+        }
+
+        private static void Delete(EventUnit unit)
+        {
+            EventManager.events.Remove(unit.Id);
+            if (EventManager.events.Count == 0) EventManager.IsWork.ReleaseMutex();
+        }
+    }
+}
--- CoreWPF/Utitltes/Event/EventUnit.cs
+++ CoreWPF/Utitltes/Event/EventUnit.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace CoreWPF.Utitltes.Event
@@ -7,36 +6,29 @@ namespace CoreWPF.Utitltes.Event
     class EventUnit
     {
         public int Id { get; private set; }
-        //private EventWaitHandle locker;
 
-        private event Action event_appFinish;
-        private event Action<EventUnit> event_finish;
-        private readonly Task job;
-
-        public Action Event_appFinish
+        private event Action<EventUnit> finish;
+        public Action<EventUnit> Finish
         {
-            get { return this.event_appFinish; }
+            get { return this.finish; }
+            set
+            {
+                this.finish = new Action<EventUnit>(value);
+            }
         }
 
-        public EventUnit(int id, Task job, Action<EventUnit> manager_action, Action end_action)
+        private readonly Task job;
+
+        public EventUnit(int id, Task job)
         {
             this.Id = id;
             this.job = job;
-            this.event_finish += manager_action;
-            this.event_appFinish += end_action;
-            //this.locker = new AutoResetEvent(false);
         }
 
         public async void Start()
         {
             await Task.Run(()=> { this.job.Start(); });
-            await Task.Run(() => { this.event_finish?.Invoke(this); });
-            //await Task.Run(() => { this.locker.Set(); });
-        }
-
-        public void WaitForEvent()
-        {
-            //this.locker.WaitOne();
+            await Task.Run(() => { this.Finish?.Invoke(this); });
         }
     }
 }
