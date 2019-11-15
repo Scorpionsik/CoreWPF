ff7b1b6993b4ca42a0c3359a75850fc3edb8d4ff
Scorpionsik <scorpionsik@gmail.com>
2019-11-15 15:49:38 +0200

    Documentation for NavigationManager

--- CoreWPF/Utilites/Navigation/NavigationManager.cs
+++ CoreWPF/Utilites/Navigation/NavigationManager.cs
@@ -9,15 +9,16 @@ namespace CoreWPF.Utilites.Navigation
 {
     /// <summary>
     /// Предоставляет инструменты для переключения нескольких ViewModel в одном окне.
-    /// <para>Начало работы: Сначала требуется создать экземпляр текущего менеджера и передать в конструкторе <see cref="Dispatcher"/> и <see cref="ContentControl"/> окна, в котором будут меняться ViewModel. 
-    /// Далее регистрируются ViewModel (в который передается ссылка на текущий менеджер), View и ключ, по которому можно будет найти ViewModel в коллекции менеджера. Рекомендуется отдельно хранить список ключей, по которым можно обратиться к тому или иному ViewModel.
-    /// После чего менеджер передается в конструкторе ViewModel основного окна.</para>
     /// </summary>
     [Serializable]
     public class NavigationManager
     {
         #region Поля и свойства
         private event Action<INavigateModule> navigation_invoke;
+
+        /// <summary>
+        /// Событие триггерится при каждом срабатывании метода <see cref="Navigate(string, object)"/>, при условии что ViewModel отнаследована от <see cref="INavigateModule"/>.
+        /// </summary>
         public Action<INavigateModule> Navigation_invoke
         {
             get { return this.navigation_invoke; }
@@ -25,26 +26,46 @@ namespace CoreWPF.Utilites.Navigation
             {
                 this.navigation_invoke = new Action<INavigateModule>(value);
             }
-        }
+        } //---свойство Navigation_invoke
 
         private readonly Dispatcher _dispatcher;
         private readonly ContentControl _frameControl;
         private readonly IDictionary<string, object> _viewModelsByNavigationKey = new Dictionary<string, object>();
         private readonly IDictionary<Type, Type> _viewTypesByViewModelType = new Dictionary<Type, Type>();
 
+        /// <summary>
+        /// Возвращает коллекцию ключей, которые были указаны при вызове метода <see cref="Register{TViewModel, TView}(TViewModel, string)"/>.
+        /// </summary>
+        public List<string> Keys
+        {
+            get
+            {
+                return new List<string>(this._viewModelsByNavigationKey.Keys);
+            }
+        } //---свойство Keys
         #endregion
 
         #region Конструкторы
+        /// <summary>
+        /// Инициализация менеджера.
+        /// <para>Начало работы: Сначала требуется создать экземпляр текущего менеджера и передать в конструкторе <see cref="Dispatcher"/> и <see cref="ContentControl"/> окна, в котором будут меняться ViewModel.
+        /// Далее регистрируются ViewModel (в который передается ссылка на текущий менеджер), View и ключ, по которому можно будет найти ViewModel в коллекции менеджера. Рекомендуется отдельно хранить список ключей, по которым можно обратиться к тому или иному ViewModel.
+        /// После чего менеджер передается в конструкторе ViewModel основного окна.</para>
+        /// Для удобства рекомендуется наследоваться от классов <see cref="NavigationViewModel"/> для вашей ViewModel и <see cref="NavigationModel"/> для вашей модели представления данных.
+        /// </summary>
+        /// <param name="dispatcher"><see cref="Dispatcher"/> окна, в котором данный диспетчер будет работать.</param>
+        /// <param name="frameControl"><see cref="ContentControl"/> окна, в котором будут переключаться ViewModel.</param>
+        /// <exception cref="ArgumentNullException"/>
         public NavigationManager(Dispatcher dispatcher, ContentControl frameControl)
         {
             if (dispatcher == null)
-                throw new ArgumentNullException("dispatcher");
+                throw new ArgumentNullException("dispatcher", "Dispatcher не указан!");
             if (frameControl == null)
-                throw new ArgumentNullException("frameControl");
+                throw new ArgumentNullException("frameControl", "FrameControl не указан!");
 
             _dispatcher = dispatcher;
             _frameControl = frameControl;
-        }
+        } //---конструктор NavigationManager
         #endregion
 
         #region Методы
@@ -65,6 +86,8 @@ namespace CoreWPF.Utilites.Navigation
         {
             if (navigationKey == null)
                 throw new ArgumentNullException("navigationKey");
+            if (!this.Keys.Contains(navigationKey))
+                throw new ArgumentException("Ключ не найден!", "navigationKey");
 
             InvokeInMainThread(() =>
             {
@@ -124,7 +147,10 @@ namespace CoreWPF.Utilites.Navigation
 
         public string GetSubtitle(string navigationKey)
         {
-            return ((INavigateModule)_viewModelsByNavigationKey[navigationKey]).Subtitle;
+            if (!this.Keys.Contains(navigationKey)) throw new ArgumentException("Ключ не найден!", "navigationKey");
+
+            if (_viewModelsByNavigationKey[navigationKey] is INavigateModule module) return module.Subtitle;
+            else throw new InvalidCastException("Выбранная ViewModel ("+ navigationKey +") не наследуется от INavigateModule!");
         }
         #endregion
     }
